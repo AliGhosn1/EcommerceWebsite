1f74a2045625ec1ddfa99b34e992488f470f6275
AliGhosn1 <alighosn2099@gmail.com>
2023-05-16 14:15:23 -0400

    Deployment VSN 1.0.1

---
+++ src/routes/category/category.styles.jsx
@@ -0,0 +1,15 @@
+import { styled } from "styled-components";
+
+export const CategoryContainer = styled.div``
+
+
+export const CategoryTitle = styled.h1`
+    text-align: center;
+`
+
+export const CategoryProductContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 10px;
+    row-gap: 50px;
+`
